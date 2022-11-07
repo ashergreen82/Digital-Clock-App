d655df8c951accac265e4d33ec1ce243f4e8f18f
Ashergreen82 <greena@rogers.com>
2022-11-07 07:03:32 -0500

    Removed all the test code
    Moved all the JavaScript and CSS code to their own files.
    Basically cleaned things up to move towards taking what I learned and molding the program into what it should be.

---
+++ index.js
@@ -0,0 +1,10 @@
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var time = new Date();
+var date = time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate();
+var myText;
+var month = time.getMonth();
+var year = time.getYear();
+var day = time.getDay();
+console.log(time);
+console.log(year, months[month], day);
+console.log(year);
