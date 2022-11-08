68c6db64d965c20864ec566c48326076354e58e7
Ashergreen82 <greena@rogers.com>
2022-11-08 06:52:32 -0500

    Trying to resolve a NULL error

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var time = new Date();
 var date = time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate();
-var myText;
 var month = time.getMonth();
-var year = time.getYear();
+var year = time.getFullYear();
 var day = time.getDay();
-console.log(time);
+console.log("time: ", time);
 console.log(year, months[month], day);
-console.log(year);
+console.log("year: ", year);
+document.getElementById("Clock").innerHTML = "This is a test";
+
