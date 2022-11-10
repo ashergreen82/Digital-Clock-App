1e4f7be6271d4399f82a1d27dba5afa48ddac987
Ashergreen82 <greena@rogers.com>
2022-11-09 21:27:20 -0500

    Fxed the syntax error

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var time = new Date();
-var date = time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate();
 var month = time.getMonth();
 var year = time.getFullYear();
 var day = time.getDay();
+var date = time.getFullYear() + '-' + months[month] + '-' + time.getDate();
 console.log("time: ", time);
 console.log(year, months[month], day);
 console.log("year: ", year);
-document.getElementById("Clock").innerHTML = "This is a test";
+document.getElementById("clock").innerHTML = date;
 
