78be2af8e385ecbaa10063b9856faf6ecf5353e5
Ashergreen82 <greena@rogers.com>
2022-11-11 06:08:07 -0500

    Added 0's as prefixes to the time
    When the time has a single digit I added a '0' in front of it.  However, there is a problem, the hour time doesn't seem to want to display the 0 unfortunatly.

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-var time = new Date();
-var month = time.getMonth();
-var year = time.getFullYear();
-var day = time.getDay();
-var hour = time.getHours();
-var minutes = time.getMinutes();
-var seconds = time.getSeconds();
-var date = time.getDate();
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const time = new Date();
+const month = time.getMonth();
+const year = time.getFullYear();
+const day = time.getDay();
+// var hour = time.getHours();
+const hour = 4;
+// var minutes = time.getMinutes();
+const minutes = 4;
+// var seconds = time.getSeconds();
+const seconds = 4;
+const date = time.getDate();
 // var date = 23;
 var number_suffix = which_number_suffix_to_use(date);
-var current_time = hour + ":" + minutes + ":" + seconds;
-var date = weekday[day] + "," + " " + months[month] + " " + date + number_suffix + " " + time.getFullYear();
+const number_prefix_hour = which_number_prefix_to_use(hour);
+const number_prefix_minutes = which_number_prefix_to_use(minutes);
+const number_prefix_seconds = which_number_prefix_to_use(seconds);
+const current_time = number_prefix_hour + hour + ":" + number_prefix_minutes + minutes + ":" + number_prefix_seconds + seconds;
+const date_display = weekday[day] + "," + " " + months[month] + " " + date + number_suffix + " " + time.getFullYear();
 document.getElementById("clock").innerHTML = current_time;
-document.getElementById("date").innerHTML = date;
+document.getElementById("date").innerHTML = date_display;
+function which_number_prefix_to_use(time) {
+    time = time.toString();
+    if (time.length < 2) {
+        number_prefix = 0
+    } else {
+        number_prefix = ""
+    }
+    return number_prefix
+}
 
 function which_number_suffix_to_use(date) {
     date = date.toString();
